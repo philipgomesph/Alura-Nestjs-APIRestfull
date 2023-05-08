3094af10ea7ac4b789ea8832acf8a057757a5c98
philipgomesph <philip.gomesph@gmail.com>
2023-05-07 23:03:31 -0300

    Iniciar projeto

---
+++ src/usuario.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsuarioRepository } from './usuario.repository';
+
+@Controller('/usuarios') // localhost:3000/usuarios
+export class UsuarioController {
+  private usuarioRepository = new UsuarioRepository();
+  @Post()
+  async criaUsuario(@Body() dadosDoUsuario) {
+    this.usuarioRepository.salvar(dadosDoUsuario);
+    return dadosDoUsuario;
+  }
+
+  @Get()
+  async listaUsuarios() {
+    return this.usuarioRepository.listaAll();
+  }
+}
---
+++ src/usuario.repository.ts
@@ -0,0 +1,11 @@
+export class UsuarioRepository {
+  private usuarios = []; // vamos trabalhar com memoria para nao ter q fazer ligação ao banco ainda
+
+  async salvar(usuario) {
+    this.usuarios.push(usuario); //Inserindo dado no array
+  }
+
+  async listaAll() {
+    return this.usuarios;
+  }
+}
